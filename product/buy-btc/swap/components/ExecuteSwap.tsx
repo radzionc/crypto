1207b43fde0f0b6b6a7350821d8447e22c35adbd
Rodion Chachura <geekrodion@gmail.com>
2025-01-07 15:53:22 +0400

    feat: enhance ExecuteSwap component with error handling and transaction feedback
    - Added error handling to display a warning when the swap transaction fails.
    - Implemented transaction success feedback with a link to track the transaction on Thorchain.
    - Introduced styled components for improved UI consistency and clarity in the swap process.

--- product/buy-btc/swap/components/ExecuteSwap.tsx
+++ product/buy-btc/swap/components/ExecuteSwap.tsx
@@ -5,31 +5,56 @@ import { toChainAmount } from '@lib/chain/utils/toChainAmount'
 import { usePresentState } from '@lib/ui/state/usePresentState'
 import { useSourceChainId } from '../state/sourceChainId'
 import { getChain } from '../../chain/config'
+import { ShyWarningBlock } from '@lib/ui/status/ShyWarningBlock'
+import { getErrorMessage } from '@lib/utils/getErrorMessage'
+import { VStack } from '@lib/ui/css/stack'
+import { text, Text } from '@lib/ui/text'
+import styled from 'styled-components'
+import { ExternalLink } from '@lib/ui/navigation/Link/ExternalLink'
 
 type ExecuteSwapProps = {
   memo: string
   receiver: `0x${string}`
 }
 
+const Link = styled(ExternalLink)`
+  ${text({ color: 'primary', weight: 'bold' })}
+`
+
 export const ExecuteSwap = ({ memo, receiver }: ExecuteSwapProps) => {
   const [sourceChainId] = useSourceChainId()
   const [amount] = usePresentState(useAmount())
-  const { sendTransaction, isPending } = useSendTransaction()
+  const { sendTransaction, isPending, error, data } = useSendTransaction()
   const { nativeCurrency } = getChain(sourceChainId)
 
   return (
-    <Button
-      kind="primary"
-      onClick={() => {
-        sendTransaction({
-          to: receiver as `0x${string}`,
-          value: toChainAmount(amount, nativeCurrency.decimals),
-          data: `0x${Buffer.from(memo).toString('hex')}`,
-        })
-      }}
-      isLoading={isPending}
-    >
-      Buy Bitcoin
-    </Button>
+    <>
+      <Button
+        kind="primary"
+        onClick={() => {
+          sendTransaction({
+            to: receiver as `0x${string}`,
+            value: toChainAmount(amount, nativeCurrency.decimals),
+            data: `0x${Buffer.from(memo).toString('hex')}`,
+          })
+        }}
+        isLoading={isPending}
+      >
+        Buy Bitcoin
+      </Button>
+      {error && (
+        <ShyWarningBlock title="Failed to execute swap">
+          {getErrorMessage(error)}
+        </ShyWarningBlock>
+      )}
+      {data && (
+        <VStack>
+          <Text>
+            Transaction sent,{' '}
+            <Link to={`https://thorchain.net/tx/${data}`}>track it here</Link>
+          </Text>
+        </VStack>
+      )}
+    </>
   )
 }
