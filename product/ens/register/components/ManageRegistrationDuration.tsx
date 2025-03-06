56254f9266778a968f31290f5867e238c2d2064a
Rodion Chachura <geekrodion@gmail.com>
2025-03-06 15:21:08 +0400

    refactor: Improve ENS Registration Duration Component UI
    - Replaced AmountInput with Slider and InputContainer for registration duration selection
    - Updated ManageRegistrationDuration to use new input components
    - Added dynamic year label display in RegistrationStepTitle
    - Enhanced visual styling and input interaction for registration duration

--- product/ens/register/components/ManageRegistrationDuration.tsx
+++ product/ens/register/components/ManageRegistrationDuration.tsx
@@ -1,4 +1,6 @@
-import { AmountInput } from '@lib/ui/inputs/Slider/AmountInput'
+import { InputContainer } from '@lib/ui/inputs/InputContainer'
+import { InputLabel } from '@lib/ui/inputs/InputLabel'
+import { Slider } from '@lib/ui/inputs/Slider'
 import { pluralize } from '@lib/utils/pluralize'
 
 import { useRegistrationDuration } from '../state/registrationDuration'
@@ -7,14 +9,18 @@ export const ManageRegistrationDuration = () => {
   const [duration, setDuration] = useRegistrationDuration()
 
   return (
-    <AmountInput
-      label="Registration duration"
-      value={duration}
-      onChange={setDuration}
-      min={1}
-      max={10}
-      step={1}
-      formatValue={(value) => pluralize(value, 'year')}
-    />
+    <InputContainer>
+      <InputLabel>
+        Registration duration: {pluralize(duration, 'year')}
+      </InputLabel>
+      <Slider
+        value={duration}
+        onChange={setDuration}
+        min={1}
+        max={10}
+        step={1}
+        size="l"
+      />
+    </InputContainer>
   )
 }
--- product/ens/register/components/RegistrationStepTitle.tsx
+++ product/ens/register/components/RegistrationStepTitle.tsx
@@ -2,5 +2,10 @@ import { text } from '@lib/ui/text'
 import styled from 'styled-components'
 
 export const RegistrationStepTitle = styled.h1`
-  ${text({ size: 24, color: 'contrast', weight: 600 })}
+  ${text({
+    size: 24,
+    color: 'contrast',
+    weight: 600,
+    centerHorizontally: true,
+  })}
 `
